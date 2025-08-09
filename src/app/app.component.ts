03c1632aee4739e77836fd9665bb0db124873773
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-08-09 22:38:08 +0700

    init(src): source-structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AddNewTodoComponent } from './components/add-new-todo/add-new-todo.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TodoListComponent, AddNewTodoComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'signal-todolist';
-}
+export class AppComponent {}
---
+++ src/app/components/add-new-todo/add-new-todo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-todo',
+  imports: [],
+  templateUrl: './add-new-todo.component.html',
+  styleUrl: './add-new-todo.component.css'
+})
+export class AddNewTodoComponent {
+
+}
